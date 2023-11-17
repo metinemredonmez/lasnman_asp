92d2bb0e44084869a19ad88a6c4d4f2dca0bb658
aknaldemir <aknaldemir@gmail.com>
2023-11-18 02:01:59 +0300

    Product create esnasında product'a ait resimlerinde eklenmesi sağlandı.

---
+++ src/lansman.Application/Product/Dto/ProductMapProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+
+namespace lansman.Product.Dto
+{
+    public class ProductMapProfile : Profile
+    {
+        public ProductMapProfile()
+        {
+
+            CreateMap<CreateProductDto, Product>()
+            .ForMember(dest => dest.ProductImages, opt => opt.MapFrom(src => src.ProductImagesDto))
+            .ReverseMap();
+
+            CreateMap<CreateProductImageDto, ProductImage>()
+                .ReverseMap();
+        }
+    }
+}
