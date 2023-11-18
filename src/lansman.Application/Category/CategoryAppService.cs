5cf026eb9e43f169107da612dce23954250b9c9e
aknaldemir <aknaldemir@gmail.com>
2023-11-19 01:28:08 +0300

    Implemented category service.

---
+++ src/lansman.Application/Category/CategoryAppService.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Application.Services;
+using lansman.Product.Dto;
+using Abp.Domain.Repositories;
+using lansman.Category.Dto;
+
+namespace lansman.Category
+{
+    public class CategoryAppService : AsyncCrudAppService<Category, CreateCategoryDto>, ICategoryAppService
+    {
+        public CategoryAppService(IRepository<Category, int> repository) : base(repository)
+        {
+        }
+    }
+}
---
+++ src/lansman.Application/Category/Dto/CategoryMapProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace lansman.Category.Dto
+{
+    public class CategoryMapProfile : Profile
+    {
+        public CategoryMapProfile()
+        {
+            CreateMap<CreateCategoryDto, Category>()
+            .ReverseMap();
+        }
+    }
+}
