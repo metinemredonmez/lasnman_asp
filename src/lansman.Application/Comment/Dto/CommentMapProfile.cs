1c5bb1b8426c9d873fdbde54d07dd6c4692637a8
aknaldemir <aknaldemir@gmail.com>
2023-11-22 00:46:48 +0300

    Comment service implement edildi.

---
+++ src/lansman.Application/Comment/Dto/CommentMapProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+
+namespace lansman.Comment.Dto
+{
+    public class CommentMapProfile : Profile
+    {
+        public CommentMapProfile()
+        {
+
+            CreateMap<CreateCommentDto, Comment>()
+            .ForMember(dest => dest.CommentImages, opt => opt.MapFrom(src => src.CommentImages))
+            .ReverseMap();
+
+            CreateMap<CreateCommentImageDto, CommentImage>()
+                .ReverseMap();
+        }
+    }
+}
---
+++ src/lansman.Application/Comment/Dto/CreateCommentImageDto.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lansman.Comment.Dto
+{
+    [AutoMapTo(typeof(CommentImage))]
+    public class CreateCommentImageDto : EntityDto
+    {
+        public byte[] Image { get; set; }
+    }
+}
